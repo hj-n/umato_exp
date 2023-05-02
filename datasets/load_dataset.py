07dbbd49e6968ba3127bd346bc6ef3f73031ee5f
Jake Hyun <jake.hyun@hotmail.com>
2023-05-03 04:39:41 +0900

    Rename modules

--- datasets/load_dataset.py
+++
@@ -1,3 +0,0 @@
-from ._load_dataset import list_datasets, load_dataset, load_all_datasets
-
-__all__ = ['list_datasets', 'load_dataset', 'load_all_datasets']
---
+++ datasets/loader.py
@@ -0,0 +1,3 @@
+from ._loader import list_datasets, load_dataset, load_all_datasets
+
+__all__ = ['list_datasets', 'load_dataset', 'load_all_datasets']
